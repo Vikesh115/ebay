bfaaf4783a53fc4842442ba255965f3958ff9274
Vikesh115 <vikeshraut952@gmail.com>
2025-05-03 11:47:33 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjQfbMmKW9J_oY--Z6trk4WoqAEWBnT9Q",
-    authDomain: "auth-99137.firebaseapp.com",
-    projectId: "auth-99137",
-    storageBucket: "auth-99137.appspot.com",
-    messagingSenderId: "915407695784",
-    appId: "1:915407695784:web:6854b358fe8d436bb7379d",
-    measurementId: "G-4SJ4Q9CZ92"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
